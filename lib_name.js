196d0afc1882fc95715687cb4995c5222b90cf5e
Jorden Vande Kop <gamingledger@gmail.com>
2024-02-25 23:48:13 -0600

    first commit

---
+++ lib_name.js
@@ -0,0 +1,11 @@
+class NameClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  logName() {
+    console.log(this.name);
+  }
+}
+
+export { NameClass };
---
+++ objects.js
@@ -0,0 +1,8 @@
+
+import { NameClass } from './lib_example.js';
+
+const obj1 = new NameClass("Alice");
+const obj2 = new NameClass("Bob");
+
+obj1.logName(); // Logs "Alice"
+obj2.logName(); // Logs "Bob"
